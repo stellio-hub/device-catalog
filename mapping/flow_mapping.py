dd01cae23544cf496428d57ac2afce0df3284121
Luc Gasser <luc.gasser@egm.io>
2024-08-02 17:23:08 +0200

    fix context management

--- mapping/flow_mapping.py
+++ mapping/flow_mapping.py
@@ -9,24 +9,23 @@ def ngsild_instance(value, time=None, unit_code=None, dataset_id=None):
         **({"unitCode": unit_code} if unit_code is not None else {}),
         **({"datasetId": dataset_id} if dataset_id is not None else {})
     }
-
     return ngsild_instance
 
-def flow_mapping(mapping_entities, source_payload):
-    ngsild_payload = []
-
-    def add_to_payload(key, value, entity_id, entity_type):
-        only_this_id = [d for d in ngsild_payload if d['id'] == entity_id]
+def add_to_payload(payload, key, value, entity_id, entity_type):
+        only_this_id = [d for d in payload if d['id'] == entity_id]
         if all(key in d for d in only_this_id):
-            ngsild_payload.append({"id": entity_id, "type": "Device", key: value})
+            payload.append({"id": entity_id, "type": "Device", key: value})
         else:
-            for d in ngsild_payload:
+            for d in payload:
                 if d['id'] == entity_id and key not in d:
                     d[key] = value
                     break
 
+def flow_mapping(mapping_entities, source_payload):
+    output = []
     for mapping in mapping_entities:
         attributes_to_map = mapping['attributesToMap'] if type(mapping['attributesToMap']) is list else [mapping['attributesToMap']]
+        ngsild_payload = []
         for item in source_payload:
             for key in item:
                 for attrs in attributes_to_map:
@@ -34,9 +33,10 @@ def flow_mapping(mapping_entities, source_payload):
                         attribute = item[key] if type(item[key]) is list else [item[key]]
                         for dataset in attribute:
                             if ("datasetId" in dataset and attrs['sourceDatasetId']['value'] == dataset['datasetId']) or ("datasetId" not in dataset and attrs['sourceDatasetId']['value'] == "@none"):
-                                add_to_payload(attrs['attribute']['value'], ngsild_instance(dataset['value'], dataset.get('observedAt'), dataset.get('unitCode'), attrs['targetDatasetId']['value']), attrs['hasTargetEntity']['object'], attrs['targetEntityType']['value'])
-
-    return ngsild_payload
+                                add_to_payload(ngsild_payload, attrs['attribute']['value'], ngsild_instance(dataset['value'], dataset.get('observedAt'), dataset.get('unitCode'), attrs['targetDatasetId']['value']), attrs['hasTargetEntity']['object'], attrs['targetEntityType']['value'])
+        if ngsild_payload != []:
+            output.append({"payload": ngsild_payload, "context": mapping['context']['value']})
+    return output
 
 def main():
     inc = json.load(sys.stdin)
