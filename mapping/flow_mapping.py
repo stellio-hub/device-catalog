a389d7ca81b131965b79e7bc2153d79c23ef031a
Luc Gasser <luc.gasser@egm.io>
2024-08-02 17:11:01 +0200

    rework flow_mapping script

--- mapping/flow_mapping.py
+++ mapping/flow_mapping.py
@@ -1,39 +1,48 @@
 import json
 import sys
 
+def ngsild_instance(value, time=None, unit_code=None, dataset_id=None):
+    ngsild_instance = {
+        "type": "Property",
+        "value": value,
+        **({"observedAt": time} if time is not None else {}),
+        **({"unitCode": unit_code} if unit_code is not None else {}),
+        **({"datasetId": dataset_id} if dataset_id is not None else {})
+    }
+
+    return ngsild_instance
+
 def flow_mapping(mapping_entities, source_payload):
-    output = []
+    ngsild_payload = []
+
+    def add_to_payload(key, value, entity_id, entity_type):
+        only_this_id = [d for d in ngsild_payload if d['id'] == entity_id]
+        if all(key in d for d in only_this_id):
+            ngsild_payload.append({"id": entity_id, "type": "Device", key: value})
+        else:
+            for d in ngsild_payload:
+                if d['id'] == entity_id and key not in d:
+                    d[key] = value
+                    break
+
     for mapping in mapping_entities:
-        payloads = []
-        maps = mapping['attributesToMap'] if type(mapping['attributesToMap']) is list else [mapping['attributesToMap']]
-        for key in source_payload:
-            for map in maps:
-                valid_payload = False
-                if key == map['value']:
-                    attribute = source_payload[key] if type(source_payload[key]) is list else [source_payload[key]]
-                    for dataset in attribute:
-                        if ("datasetId" in dataset and map['sourceDatasetId']['value'] == dataset['datasetId']) or ("datasetId" not in dataset and map['sourceDatasetId']['value'] == "@none"):
-                            payload = {
-                                "id": map['hasTargetEntity']['object'],
-                                "type": map['targetEntityType']['value'],
-                                map['attribute']['value']: dataset.copy()
-                            }
-                            payload[map['attribute']['value']]['datasetId'] = map['targetDatasetId']['value']
-                            if 'instanceId' in payload[map['attribute']['value']]:
-                                del payload[map['attribute']['value']]['instanceId']
-                            valid_payload = True
-                if valid_payload:
-                    payloads.append(payload)
-        if payloads != []:
-            output.append({"payload": payloads, "context": mapping['context']['value']})
-    return output
+        attributes_to_map = mapping['attributesToMap'] if type(mapping['attributesToMap']) is list else [mapping['attributesToMap']]
+        for item in source_payload:
+            for key in item:
+                for attrs in attributes_to_map:
+                    if key == attrs['value']:
+                        attribute = item[key] if type(item[key]) is list else [item[key]]
+                        for dataset in attribute:
+                            if ("datasetId" in dataset and attrs['sourceDatasetId']['value'] == dataset['datasetId']) or ("datasetId" not in dataset and attrs['sourceDatasetId']['value'] == "@none"):
+                                add_to_payload(attrs['attribute']['value'], ngsild_instance(dataset['value'], dataset.get('observedAt'), dataset.get('unitCode'), attrs['targetDatasetId']['value']), attrs['hasTargetEntity']['object'], attrs['targetEntityType']['value'])
+
+    return ngsild_payload
 
 def main():
-    input = json.load(sys.stdin)
-    source_payload = input[0]['sourcePayload']
-    output = flow_mapping(input, source_payload)
-    json.dump(output, sys.stdout, indent=4)
+    inc = json.load(sys.stdin)
+    source_payload = source_payload = inc[0]['sourcePayload'] if type(inc[0]['sourcePayload']) is list else [inc[0]['sourcePayload']]
+    out = flow_mapping(inc, source_payload)
+    json.dump(out, sys.stdout, indent=4)
 
 if __name__ == "__main__":
     main()
-  
