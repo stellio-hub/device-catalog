8ba1abaa8874e3261ec273deea58d784f46c0504
Lea Robert (EGM) <lea.robert@egm.io>
2025-02-21 15:37:48 +0100

    #5851 Added EM310-TILT decoder

---
+++ manufacturers/Milesight/models/EM310-TILT/uplink_decoder.js
@@ -0,0 +1,25 @@
+let codec = require("./decode.js")
+let ngsild = require("../ngsi-ld.js")
+
+let parametersMapping =  {
+    sn: {label:"serialNumber", unitCode: "", datasetId: null},
+    hardware_version: {label:"hardwareVersion", unitCode: "", datasetId: null},
+    software_version: {label:"softwareVersion", unitCode: "", datasetId: null},
+    battery: {label:"batteryLevel", unitCode: "P1", datasetId: null},
+    angle_x: {label:"angleX", unitCode: "DD", datasetId: null},
+    angle_y: {label:"angleY", unitCode: "DD", datasetId: null},
+    angle_z: {label:"angleZ", unitCode: "DD", datasetId: null}
+}
+
+function main() {
+    var payload = process.argv[3];
+    var time = process.argv[4];
+    var entity_id = "urn:ngsi-ld:Device:" + process.argv[5];
+    var decoded = codec.decode(Buffer.from(payload, 'hex'));
+    var ngsild_payload = ngsild.ngsildWrapper(decoded, time, entity_id, parametersMapping);
+    process.stdout.write(JSON.stringify(ngsild_payload, null, 2));
+}
+
+if (require.main === module) {
+    main();
+}
