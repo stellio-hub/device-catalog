8661f1d2ca140667800f9939b88e0e0b6d633fcd
Lea Robert (EGM) <lea.robert@egm.io>
2025-02-21 14:21:21 +0100

    #5405 Added AM300 decoder

---
+++ manufacturers/Milesight/models/AM300/uplink_decoder.js
@@ -0,0 +1,32 @@
+let codec = require("./decode.js")
+let ngsild = require("../ngsi-ld.js")
+
+let parametersMapping =  {
+    sn: {label:"serialNumber", unitCode: "", datasetId: null},
+    hardware_version: {label:"hardwareVersion", unitCode: "", datasetId: null},
+    software_version: {label:"softwareVersion", unitCode: "", datasetId: null},
+    temperature: {label:"temperature", unitCode: "CEL", datasetId: 'Raw'},
+    humidity: {label:"humidity", unitCode: "P1", datasetId: 'Raw'},
+    light_level: {label:"lightLevel", unitCode: "", datasetId: 'Raw'},
+    // 0：0-5lux, 01：6-50lux, 2：51-100lux, 3：101-500lux, 4：501-2000lux, 5：>2000lux
+    co2: {label:"co2", unitCode: "59", datasetId: 'Raw'}, // "59" is the unit code for ppm
+    tvoc: {label:"tvoc", unitCode: "", datasetId: 'Raw'},
+    pressure: {label:"pressure", unitCode: "P1", datasetId: 'Raw'},
+    hcho: {label:"hcho", unitCode: "GP", datasetId: 'Raw'}, // "GP" is the unit code for mg/m3
+    pm2_5: {label:"pm25", unitCode: "GQ", datasetId: 'Raw'}, // "GQ" is the unit code for μg/m3
+    pm10: {label:"pm10", unitCode: "GQ", datasetId: 'Raw'},
+    o3: {label:"o3", unitCode: "59", datasetId: 'Raw'}
+}
+
+function main() {
+    var payload = process.argv[3];
+    var time = process.argv[4];
+    var entity_id = "urn:ngsi-ld:Device:" + process.argv[5];
+    var decoded = codec.decode(Buffer.from(payload, 'hex'));
+    var ngsild_payload = ngsild.ngsildWrapper(decoded, time, entity_id, parametersMapping);
+    process.stdout.write(JSON.stringify(ngsild_payload, null, 2));
+}
+
+if (require.main === module) {
+    main();
+}
