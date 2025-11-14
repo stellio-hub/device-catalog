b5680c60a15b1c0c08e42aaf47e97cc367cb11ac
nikolaskyr <45922282+nikolaskyr@users.noreply.github.com>
2025-11-14 12:30:50 +0200

    Store battery information for Sensecap S210X

--- manufacturers/SenseCAP/models/S2105/uplink_decoder.js
+++ manufacturers/SenseCAP/models/S2105/uplink_decoder.js
@@ -332,6 +332,9 @@ function ngsildWrapper(input, time, entity_id) {
                     ngsild_payload[0].soilElectricalConductivity = ngsildInstance(messages[i].measurementValue, time, 'H61', 'Raw');
                 }
             }
+			if (messages[i].type === 'upload_battery') {
+				ngsild_payload[0].battery = ngsildInstance(messages[i].battery, time, 'P1', 'Raw');
+			}
         }
     }
     return ngsild_payload;                                                                                                                                                                                                                                                                                                                                          
