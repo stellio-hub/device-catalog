7f812ae213428d1ee7e6237bd460a7cc282ae407
Luc Gasser <luc.gasser@egm.io>
2026-08-11 12:07:08 +0200

    feat: handle multiple services

--- manufacturers/EGM/models/EdgeSpotLite-SoilSense/downlink_encoder.js
+++ manufacturers/EGM/models/EdgeSpotLite-SoilSense/downlink_encoder.js
@@ -3,17 +3,26 @@ const fs = require("fs");
 function encodeMeasurementFrequency(frequency) {
   const payload = Buffer.alloc(2);
   payload.writeUInt16BE(frequency);
-
   return {
     data: payload.toString("base64"),
     port: 8,
   };
 }
 
-const command = JSON.parse(fs.readFileSync(0, "utf8"));
+function main() {
+  const command = JSON.parse(fs.readFileSync(0, "utf8"));
+  const serviceName = process.argv[2];
+  switch (serviceName) {
+    case "setMeasurementFrequency":
+      const frequency = Number(command.measurementFrequency.value);
+      const result = encodeMeasurementFrequency(frequency);
+      process.stdout.write(JSON.stringify(result));
+      break;
+    default:
+      throw new Error("Unsupported service");
+  }
+}
 
-const frequency = Number(command.measurementFrequency.value);
-
-const result = encodeMeasurementFrequency(frequency);
-
-process.stdout.write(JSON.stringify(result));
+if (require.main === module) {
+  main();
+}
