dbbda035813a1bb3f6df452a397536fff493a1a8
ihebkhelifi <55023181+ihebkhelifi@users.noreply.github.com>
2026-08-21 15:46:05 +0200

    Feat/add citylone uplink decoder (#110)
    * add setRejoinDate service
    
    * add reset service
    
    * add citylone uplink decoder

--- manufacturers/EGM/models/EdgeSpotLite-SoilSense/downlink_encoder.js
+++ manufacturers/EGM/models/EdgeSpotLite-SoilSense/downlink_encoder.js
@@ -9,6 +9,24 @@ function encodeMeasurementFrequency(frequency) {
   };
 }
 
+function encodeRejoinTime(timestamp) {
+  const payload = Buffer.alloc(4);
+  payload.writeUInt32BE(timestamp);
+  return {
+    data: payload.toString("base64"),
+    port: 5,
+  };
+}
+
+function encodeReset() {
+  const payload = Buffer.alloc(1);
+  payload.writeUInt8(Math.floor(Math.random() * 256));
+  return {
+    data: payload.toString("base64"),
+    port: 7,
+  };
+}
+
 function main() {
   const command = JSON.parse(fs.readFileSync(0, "utf8"));
   const serviceName = process.argv[2];
@@ -17,6 +35,13 @@ function main() {
       const frequency = Number(command.measurementFrequency.value);
       const result = encodeMeasurementFrequency(frequency);
       process.stdout.write(JSON.stringify(result));
+      break;
+    case "setRejoinTime":
+      const timestamp = Number(command.rejoinTime.value);
+      process.stdout.write(JSON.stringify(encodeRejoinTime(timestamp)));
+      break;
+    case "reset":
+      process.stdout.write(JSON.stringify(encodeReset()));
       break;
     default:
       throw new Error("Unsupported service");
